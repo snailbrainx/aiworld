480bd14298e27421836b6becfe3307299f63af11
snailbrain <acoloss@hotmail.com>
2024-04-10 18:48:01 +0100

    remove unwanted

--- database/aiworld_schema.sql
+++ database/aiworld_schema.sql
@@ -1,19 +1,3 @@
--- --------------------------------------------------------
--- Host:                         xx
--- Server version:               10.8.3-MariaDB-1:10.8.3+maria~jammy - mariadb.org binary distribution
--- Server OS:                    debian-linux-gnu
--- HeidiSQL Version:             12.0.0.6536
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 -- Dumping database structure for aiworld
 CREATE DATABASE IF NOT EXISTS `aiworld` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
@@ -36,7 +20,6 @@ CREATE TABLE IF NOT EXISTS `aiworld` (
   KEY `time` (`time`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3193 DEFAULT CHARSET=utf8mb4;
 
--- Data exporting was unselected.
 
 -- Dumping structure for table aiworld.entities
 CREATE TABLE IF NOT EXISTS `entities` (
@@ -51,10 +34,3 @@ CREATE TABLE IF NOT EXISTS `entities` (
   KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
